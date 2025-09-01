f65de877fc77c09f6f3c8c39e9e645a8dd92ae0c
Schiggy <hillbrecht.s@gmail.com>
2025-09-01 22:30:58 +0200

    added generated sector data to visible map and added svgs for stars and black holes

--- src/components/map/board/Board.jsx
+++ src/components/map/board/Board.jsx
@@ -1,30 +1,20 @@
 import "./board.css";
 import Field from "../field/Field";
+import { generateSector } from "../../../utils/generateSector";
 
 export default function Board() {
-  const rows = 10;
-  const columns = 8;
-  const fieldsArray = [];
-
-  for (let row = 0; row < rows; row++) {
-    for (let column = 0; column < columns; column++) {
-      fieldsArray.push({
-        row: row,
-        column: column,
-      });
-    }
-  }
+  const sector = generateSector();
 
   return (
     <div
       className="board"
       style={{
-        gridTemplateColumns: `repeat(${columns * 4 + 2} , 1fr)`,
-        gridTemplateRows: `repeat(${rows * 3 + 1}, 1fr)`,
+        gridTemplateColumns: `repeat(${sector.columns * 4 + 2} , 1fr)`,
+        gridTemplateRows: `repeat(${sector.rows * 3 + 1}, 1fr)`,
       }}
     >
-      {fieldsArray.map((fieldData, index) => (
-        <Field key={index} fieldData={fieldData} index={index} />
+      {sector.fields.map((field, index) => (
+        <Field key={index} field={field} index={index} />
       ))}
     </div>
   );
--- src/components/map/field/Field.jsx
+++ src/components/map/field/Field.jsx
@@ -1,23 +1,61 @@
 import "./Field.css";
 
-export default function Field({ fieldData, index }) {
+export default function Field({ field, index }) {
   const getColumnStart = () => {
-    const offset = fieldData.row % 2 !== 0 ? 2 : 0;
-    return fieldData.column * 4 + offset + 1;
+    const offset = field.row % 2 !== 0 ? 2 : 0;
+    return field.column * 4 + offset + 1;
   };
 
   const getColumnEnd = () => {
-    const offset = fieldData.row % 2 !== 0 ? 2 : 0;
-    return fieldData.column * 4 + offset + 5;
+    const offset = field.row % 2 !== 0 ? 2 : 0;
+    return field.column * 4 + offset + 5;
   };
 
   const getRowStart = () => {
-    return fieldData.row * 4 + 1 - fieldData.row;
+    return field.row * 4 + 1 - field.row;
   };
 
   const getRowEnd = () => {
-    return fieldData.row * 4 + 5 - fieldData.row;
+    return field.row * 4 + 5 - field.row;
   };
+
+  const blackHole = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      className="field-svg"
+    >
+      <g stroke="#fff" stroke-width="1">
+        <circle cx="12" cy="12" r="2" />
+        <path stroke-linecap="round" d="M12 10c5 0 4.6 12-3 12" />
+        <path stroke-linecap="round" d="M12.312 14c-5 0-4.6-12 3-12" />
+        <path
+          stroke-linecap="round"
+          d="M10 12.312c0-2.78 3.707-3.89 7-3.024m5 6.024c0-1.97-.806-3.456-2-4.49M14 12c0 2.779-3.707 3.89-7 3.024M2 9c0 1.68.586 3.008 1.5 4.004"
+        />
+      </g>
+    </svg>
+  );
+
+  const star = (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <path
+        stroke="#fff"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="1"
+        d="m12 3 1.43 5.312c.175.648.262.972.434 1.237a2 2 0 0 0 .587.587c.265.172.589.26 1.237.434L21 12l-5.312 1.43c-.648.175-.972.262-1.237.434a1.999 1.999 0 0 0-.587.587c-.172.265-.26.589-.434 1.237L12 21l-1.43-5.312c-.175-.648-.262-.972-.434-1.237a2 2 0 0 0-.587-.587c-.265-.172-.589-.26-1.237-.434L3 12l5.312-1.43c.648-.175.972-.262 1.237-.434a2 2 0 0 0 .587-.587c.172-.265.26-.589.434-1.237L12 3Z"
+      />
+    </svg>
+  );
+
+  let svg = null;
+  if (field.type === "Star") {
+    svg = star;
+  } else if (field.type === "Black Hole") {
+    svg = blackHole;
+  }
 
   return (
     <div
@@ -31,6 +69,7 @@ export default function Field({ fieldData, index }) {
     >
       <div className="field-inside"></div>
       <p className="field-number">{index + 1}</p>
+      {svg}
     </div>
   );
 }
