dcc8632c66d8a6111459659d6b780b2fe2a23d58
Schiggy <hillbrecht.s@gmail.com>
2025-09-01 16:59:07 +0200

    Moved Board into its own folder

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,31 +1,10 @@
-import "./Map.css";
-import Field from "../field/Field";
+import "./map.css";
+import Board from "./board/Board";
 
-export default function HexMap() {
-  const rows = 10;
-  const columns = 8;
-  const fieldsArray = [];
-
-  for (let row = 0; row < rows; row++) {
-    for (let column = 0; column < columns; column++) {
-      fieldsArray.push({
-        row: row,
-        column: column,
-      });
-    }
-  }
-
+export default function Map() {
   return (
-    <div
-      className="map"
-      style={{
-        gridTemplateColumns: `repeat(${columns * 4 + 2} , 1fr)`,
-        gridTemplateRows: `repeat(${rows * 3 + 1}, 1fr)`,
-      }}
-    >
-      {fieldsArray.map((fieldData, index) => (
-        <Field key={index} fieldData={fieldData} index={index} />
-      ))}
-    </div>
+    <dvi className="map">
+      <Board />
+    </dvi>
   );
 }
---
+++ src/components/map/board/Board.jsx
@@ -0,0 +1,31 @@
+import "./board.css";
+import Field from "../field/Field";
+
+export default function Board() {
+  const rows = 10;
+  const columns = 8;
+  const fieldsArray = [];
+
+  for (let row = 0; row < rows; row++) {
+    for (let column = 0; column < columns; column++) {
+      fieldsArray.push({
+        row: row,
+        column: column,
+      });
+    }
+  }
+
+  return (
+    <div
+      className="board"
+      style={{
+        gridTemplateColumns: `repeat(${columns * 4 + 2} , 1fr)`,
+        gridTemplateRows: `repeat(${rows * 3 + 1}, 1fr)`,
+      }}
+    >
+      {fieldsArray.map((fieldData, index) => (
+        <Field key={index} fieldData={fieldData} index={index} />
+      ))}
+    </div>
+  );
+}
