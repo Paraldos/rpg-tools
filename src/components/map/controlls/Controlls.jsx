4ee689d5b59ee5d96999e854d65b109e36657f1e
Schiggy <hillbrecht.s@gmail.com>
2025-09-01 17:06:56 +0200

    Added placeholder buttons to controlls and some basic styling

--- src/components/map/controlls/Controlls.jsx
+++ src/components/map/controlls/Controlls.jsx
@@ -1,5 +1,12 @@
 import "./controlls.css";
 
 export default function Controlls() {
-  return <div className="controlls">Bert</div>;
+  return (
+    <div className="controlls">
+      <button>Load / Save</button>
+      <button>New</button>
+      <button>Export</button>
+      <button>Import</button>
+    </div>
+  );
 }
