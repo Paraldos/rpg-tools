24621f768d3d8c05a51d8e772587eb6a63f530c4
Schiggy <hillbrecht.s@gmail.com>
2025-09-01 19:38:03 +0200

    added fonts and made new font montserrat default

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,9 +1,11 @@
 import "./map.css";
 import Board from "./board/Board";
+import Controlls from "./controlls/Controlls";
 
 export default function Map() {
   return (
     <dvi className="map">
+      <Controlls />
       <Board />
     </dvi>
   );
